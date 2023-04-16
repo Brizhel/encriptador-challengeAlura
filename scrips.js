1ef8224e427a11b80a74c06c1870554575723ad7
Reny David Mireles Bozo <renymirelesd@gmail.com>
2023-04-16 18:00:27 -0400

    versión 1.0 del encriptador

--- scrips.js
+++ scrips.js
@@ -5,6 +5,12 @@ var errorSpan = document.getElementById("formError");
 salida = document.getElementById("salida")
 function copiar(){
     navigator.clipboard.writeText(salida.value)
+    errorSpan.innerHTML = "&#10003 Mensaje copiado"
+    setTimeout(mostrarMensaje,2000)
+
+}
+function mostrarMensaje(){
+    errorSpan.innerHTML = "";
 }
 function encriptar (){
     var texto = document.querySelector("#inputT").value;
@@ -15,13 +21,14 @@ function encriptar (){
         dibujo.style.display = 'none';
         botoncopy.style.display = 'initial';
         hide.style.display = 'none';
-        errorSpan.innerHTML = "" ;// plain javascript
+        errorSpan.innerHTML = "" ;
     }
     else{
         dibujo.style.display = ''
         hide.style.display = '';
         botoncopy.style.display = 'none';
-        errorSpan.innerHTML = "🚫 Agregue un texto para encriptar" // plain javascript
+        errorSpan.innerHTML = "&#10060 Agregue un texto para encriptar"
+        setTimeout(mostrarMensaje,2000)
     }
 }
 function desencriptar (){ 
@@ -34,7 +41,7 @@ function desencriptar (){
     }
     else{
         dibujo.style.display = '';
-        errorSpan.innerHTML = "🚫 Agregue un texto para desencriptar";
+        errorSpan.innerHTML = "&#10060 Agregue un texto para desencriptar";
     }
     document.querySelector("#inputT").value = "";
 }
